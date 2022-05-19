46ee77ccd607cea2e042e8d0b26f57fdab8945b0
thomas-vdz <thomasvanderz@live.nl>
2022-05-19 16:52:11 +0200

    Version 1.0 - Basic market with ZI trader

--- CDA.py
+++ CDA.py
@@ -7,7 +7,7 @@ Created on Tue May  3 10:22:34 2022
 import csv
 import random
 from tqdm import tqdm
-
+from copy import deepcopy
 
 class Order:
     
@@ -33,7 +33,6 @@ class Order:
 
 
 class Orderbook:
-    #Define min/max price so 
 
     def __init__(self):
         #Limit order book: Dictionary, 
@@ -48,7 +47,7 @@ class Orderbook:
 
     
     def anon_lob(self):
-        #Test if there is a best-ask/bid present and report it in the anon_lob else return a empty tuple
+        #Test if there is a best-ask/bid present and report it in the anon_lob else return a empty list
         #For loop to avoid writing the same line 4 times
         for pair in [("X","bid"),("X","ask"),("Y","bid"),("Y","ask")]:
             
@@ -82,7 +81,13 @@ class Orderbook:
                     pass
         #Update the anonymous lob
         self.anon_lob()
-             
+    
+    def del_order_lob(self, ptype, otype):
+        
+        del self.lob[ptype][otype]
+        #Update the anonymous lob
+        self.anon_lob()        
+    
     
     
     
@@ -91,21 +96,21 @@ class Orderbook:
 class Exchange(Orderbook):
             
     
-    def __init__(self):
+    def __init__(self, traders):
         self.minprice = 1  # minimum price in the system, in cents/pennies
         self.maxprice = 200  # maximum price in the system, in cents/pennies
-        
+        self.traders = traders
         self.ob = Orderbook() #Orderbook
         
     
-    def process_order(self, time, order, balance):        
+    def process_order(self, time, order):        
         trade = None
         
         if order.otype == "ask":
             #Check if they have enough to post the offer
-            if order.quantity <= balance[order.ptype]:
+            if order.quantity <= traders[order.tid].balance[order.ptype]:
                 
-                #Set the floor bid to minimum price if there is no current best floor 
+                #Set the floor ask to maximum price if there is no current best floor 
                 #To prevent the comparison to see if ask crosses bid to fail
                 if self.ob.lob[order.ptype].get("bid") is None:
                     floorprice = self.minprice
@@ -113,47 +118,57 @@ class Exchange(Orderbook):
                     floorprice = self.ob.lob[order.ptype].get("bid").price
                     
                 
-                #Check if the ask crosses the bid if so trade can occur else add the order to the book
+                #Check if the bid crosses the ask
                 if order.price <= floorprice:
                     
+
                     #Get ID of counterparty                                       
                     buyer_id = self.ob.lob[order.ptype]["bid"].tid 
                     seller_id = order.tid
                     
-                    #Partial sell: update quantity
-                    if order.quantity < self.ob.lob[order.ptype]["bid"].quantity:
-                        self.ob.lob[order.ptype]["bid"].quantity -= order.quantity                        
-                        quant_sold = order.quantity
-                        price_sold = self.ob.lob[order.ptype]["ask"].price
+                    #Check if counterparty still holds the money to complete the trade else delete their old offer
+                    if ( self.ob.lob[order.ptype]["bid"].price * order.quantity) <= traders[buyer_id].balance["money"]:
                         
-                    
-                    #Full sell: remove order
-                    else:                        
-                        #If we offer more than is able to be bought do we create a new transaction and give it as best bid?
-                        quant_sold = self.ob.lob[order.ptype]["bid"].quantity
-                        price_sold = self.ob.lob[order.ptype]["bid"].price
-                        del self.ob.lob[order.ptype]["bid"]
-                    
-                    #Create trade for book keeping (we might add type here if we want to save other transactions like cancellations)                
-                    trade = {"time" : time,
-                             "buyer_id" : buyer_id,
-                             "seller_id" : seller_id,
-                             "price" : price_sold,
-                             "quantity" : quant_sold,
-                             "ptype" : order.ptype
-                             }
-                    
-                    return trade
-                                                        
+                        #Partial sell: update quantity
+                        if order.quantity < self.ob.lob[order.ptype]["bid"].quantity:
+                            self.ob.lob[order.ptype]["bid"].quantity -= order.quantity                        
+                            quant_sold = order.quantity
+                            price_sold = self.ob.lob[order.ptype]["bid"].price
+                            
+                        
+                        #Full sell: remove order
+                        else:                        
+                            
+                            quant_sold = self.ob.lob[order.ptype]["bid"].quantity
+                            price_sold = self.ob.lob[order.ptype]["bid"].price
+                            self.ob.del_order_lob(order.ptype, "bid")
+                        
+                        #Create trade for book keeping (we might add type here if we want to save other transactions like cancellations)                
+                        trade = {"time" : time,
+                                 "buyer_id" : buyer_id,
+                                 "seller_id" : seller_id,
+                                 "price" : price_sold,
+                                 "quantity" : quant_sold,
+                                 "ptype" : order.ptype
+                                 }
+                        
+                        return trade
+                    else:
+                        #Delete offer counterparty 
+                        self.ob.del_order_lob(order.ptype, "bid")
+                        #Add as regular offer
+                        self.ob.add_order_lob(order)
+                                                           
                 else:
                     self.ob.add_order_lob(order)
         
             else:
+                #Add functionality here to record traders posting infeasible bids
                 print("Not enough goods")
                 
         elif order.otype == "bid":
             #Check if they have enough money to post the bid
-            if (order.price * order.quantity) <= balance["money"]:
+            if (order.price * order.quantity) <= traders[order.tid].balance["money"]:
                                  
                 #Set the floor ask to maximum price if there is no current best floor 
                 #To prevent the comparison to see if bid crosses ask to fail
@@ -162,38 +177,46 @@ class Exchange(Orderbook):
                 else:
                     floorprice = self.ob.lob[order.ptype].get("ask").price
                     
-                #Check if the bid crosses the ask
+                #Check if the bid crosses the ask if the floor offer does not exist give maxprice
                 if order.price >= floorprice:
                     #Get id of counterparty
                     seller_id = self.ob.lob[order.ptype]["ask"].tid 
                     buyer_id  = order.tid
-                    #Partial buy: update quantity
-                    if order.quantity < self.ob.lob[order.ptype]["ask"].quantity:
-                        self.ob.lob[order.ptype]["ask"].quantity -= order.quantity                        
-                        quant_sold = order.quantity
-                        price_sold = self.ob.lob[order.ptype]["ask"].price
+                    
+                    #Check if counterparty still holds the goods to complete the trade else delete their old offer
+                    if (order.quantity) <= traders[seller_id].balance[order.ptype]:
+                        #Partial buy: update quantity
+                        if order.quantity < self.ob.lob[order.ptype]["ask"].quantity:
+                            self.ob.lob[order.ptype]["ask"].quantity -= order.quantity                        
+                            quant_sold = order.quantity
+                            price_sold = self.ob.lob[order.ptype]["ask"].price
+                            
                         
-                    
-                    #Full buy: remove order
-                    else:                        
-                        #If we offer more than is able to be bought do we create a new transaction and give it as best ask
-                        quant_sold = self.ob.lob[order.ptype]["ask"].quantity
-                        price_sold = self.ob.lob[order.ptype]["ask"].price
-                        del self.ob.lob[order.ptype]["ask"]
-                    
-                    #Create trade for book keeping (we might add type here if we want to save other transactions like cancellations)                
-                    trade = {"time" : time,
-                             "buyer_id" : buyer_id,
-                             "seller_id" : seller_id,
-                             "price" : price_sold,
-                             "quantity" : quant_sold,
-                             "ptype" : order.ptype
-                             }
-                    return trade                                     
-                
+                        #Full buy: remove order
+                        else:                        
+                            
+                            quant_sold = self.ob.lob[order.ptype]["ask"].quantity
+                            price_sold = self.ob.lob[order.ptype]["ask"].price
+                            self.ob.del_order_lob(order.ptype, "ask")
+                        
+                        #Create trade for book keeping (we might add type here if we want to save other transactions like cancellations)                
+                        trade = {"time" : time,
+                                 "buyer_id" : buyer_id,
+                                 "seller_id" : seller_id,
+                                 "price" : price_sold,
+                                 "quantity" : quant_sold,
+                                 "ptype" : order.ptype
+                                 }
+                        return trade                                     
+                    else:
+                        #Delete offer counterparty 
+                        self.ob.del_order_lob(order.ptype, "ask")
+                        #Add as regular offer
+                        self.ob.add_order_lob(order)
                 else:
                     self.ob.add_order_lob(order)
             else:
+                #Add functionality here to record traders posting infeasible bids
                 print("Not enough money")
         else:
             raise ValueError("Offer was neither a bid nor an ask")
@@ -205,10 +228,10 @@ class Exchange(Orderbook):
         return self.ob.alob
     
 
-    
 
-   
+#--------------------------- Traders -------------------------------------------------
 
+#Main trader class
 class Trader:
     
     def __init__(self, tid, ttype, talgo, balance):
@@ -219,10 +242,11 @@ class Trader:
         self.talgo = talgo #String: What kind of trader it is: ZIP,ZIC eGD etc
         self.balance = balance #Dictionary containing the balance of the trader
         self.blotter = [] #List of executed trades
-        self.utility = self.calc_utility()
+        self.utility = 0 #Utility level of the trader
         
         
-        
+    def __str__(self):
+        return f"Trader{self.tid}: Money:{self.balance['money']}, X:{self.balance['X']}, Y, {self.balance['Y']}"    
         
     def calc_utility(self):
         """Function returning the current utility level given the balance and trader type"""
@@ -239,25 +263,27 @@ class Trader:
     
     
     def bookkeep(self, trade):
-        
+        """Updates the balance of the trader and adds the trade to the blotter """
         #Add the transaction to blotter
         self.blotter.append(trade)
         
-        
+        #If trader and seller are the same do nothing
+        if trade["buyer_id"] == trade["seller_id"]:
+            pass
         #Check if its the buyer or seller of the trade and update balances
-        if trade["buyer_id"] == self.tid:
+        elif trade["buyer_id"] == self.tid:
             
             #Add goods of correct type
             self.balance[ trade["ptype"] ] += trade["quantity"]
             #Subtract money
-            self.balance["money"] -= trade["quantity"] * trade["price"]
+            self.balance["money"] -= (trade["quantity"] * trade["price"])
             
         elif trade["seller_id"] == self.tid:
             
             #Subtract goods of correct type
             self.balance[ trade["ptype"] ] -= trade["quantity"]
             #Add money
-            self.balance["money"] += trade["quantity"] * trade["price"]
+            self.balance["money"] += (trade["quantity"] * trade["price"])
             
         else:
             raise ValueError("Trader was not involved in this trade")
@@ -298,7 +324,7 @@ class Trader_ZI(Trader):
             #Choose to bid or ask
             action = random.choice(["bid", "ask"])
             
-            #If bid choose a random good to bid on choose a price 
+            #If bid choose a random good to bid on and choose a random price 
             if action == "bid":
                 good = random.choice(["X", "Y"])
                 #Choose random price max is maxprice or money left whatever is less
@@ -348,8 +374,8 @@ def create_csv(file_loc, dictionaries):
     
     #ADD CHECK TO SEE IF FILE ALREADY EXISTS ELSE CREATE A NEW ONE
     
-    #Create file in results folder with write access 
-    file = open("results\\"+file_loc, "w")
+    #Create csv file with write access 
+    file = open(file_loc, "w", newline='')
     
     dict_writer = csv.DictWriter(file, keys)
     dict_writer.writeheader()
@@ -385,13 +411,15 @@ def trader_type(tid, ttype, talgo):
 
 endtime = 3000
 
+
 #History of all succesfull trades
 history = []
-
-exchange = Exchange()
+orders = []
 
 time = 1 
+order_id = 1
 
+#Dictionary of traders indexed by unique trader id
 traders = {}
 
 #Create 15 ZI traders
@@ -400,6 +428,10 @@ for i in range(5):
     for j in [1,2,3]:
         traders[trader_id] = trader_type(trader_id, j, "ZI") 
         trader_id += 1 
+
+exchange = Exchange(traders)
+
+
 
 for i in tqdm(range(endtime)):
     
@@ -410,6 +442,10 @@ for i in tqdm(range(endtime)):
     trader_list = [i for i in range(1, len(traders)+1)]
 
     while len(trader_list) != 0:
+        #Reset variables
+        trade = None
+        order = None
+        
         #Choose random trader to act        
         tid = random.choice( trader_list )
         #Remove that trader from the temporary list
@@ -422,31 +458,41 @@ for i in tqdm(range(endtime)):
         lob = exchange.publish_alob()
         order = trader.get_order(time, lob)
         
-        
-        #Process the order
-        trade = exchange.process_order(time, order, trader.balance)
-        
-    
-        #Check if trade has occurred
-        if trade is not None:
+     
+        #Check if trader gave an order
+        if order:
+            #Give order unique id and increment
+            order.oid = order_id
+            order_id += 1
             
-            #Check if they buy from themselves and ignore the order
-            if trade["seller_id"] == trade["buyer_id"]:
-                pass
-            else:
+            #Add order to the list 
+            orders.append(deepcopy(order))
+            
+            #Process the order
+            trade = exchange.process_order(time, order)
+        
+
+            #Check if trade has occurred
+            if trade is not None:
                 #Update the balance and utility of the parties involved after the trade 
                 #and save the current values of their balance and utility level after the trade
                 seller_balance, seller_util = traders[ trade["seller_id"] ].bookkeep(trade)
                 buyer_balance, buyer_util = traders[ trade["buyer_id"] ].bookkeep(trade)    
+                
                 
                 #Add updated information to the trade
                 trade["seller_util"] = seller_util
                 trade["seller_balance"] = seller_balance
                 trade["buyer_util"] = buyer_util
                 trade["buyer_balance"] = buyer_balance
-                
-                #Append it to the history
-                history.append(trade)        
+    
+                #Append it to the history using deepcopy 
+                history.append(deepcopy(trade))   
+                if (seller_balance["money"] < 0 or buyer_balance["money"] < 0):
+                    raise ValueError("money negative")
+        else:
+            pass
     time += 1
 
-#create_csv("test.csv", history)
+
+create_csv("test.csv", history)
